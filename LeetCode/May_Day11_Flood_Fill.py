f7bd652a0b4d9d2daf198e25f67de9e0af62bee2
fysh711426 <fysh711426@hotmail.com>
2021-04-26 04:29:20 +0800

    update May_Day11_Flood_Fill

--- LeetCode/May_Day11_Flood_Fill.py
+++ LeetCode/May_Day11_Flood_Fill.py
@@ -1,25 +1,27 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        # 上下左右的偏移量
-        dir_r, dir_c = [-1, 1, 0, 0], [0, 0, -1, 1]
         # 圖片長寬
         rlen, clen = len(image), len(image[0])
         # 起始點顏色
         oldColor = image[sr][sc]
         
         def dfs(r, c):
+            # 判斷是否超出邊界
+            if r < 0 or r >= rlen or c < 0 or c >= clen:
+                return
+            # 檢查顏色是否和起始點相同
             if image[r][c] != oldColor:
                 return
+            # 檢查是否已填色過
             if image[r][c] == newColor:
                 return
+            # 修改顏色
             image[r][c] = newColor
-            # 往四個方向找色
-            for i in range(4):
-                # 產生新座標
-                nr, nc = r + dir_r[i], c + dir_c[i]
-                # 判斷是否超出邊界
-                if 0 <= nr < rlen and 0 <= nc < clen:
-                    dfs(nr, nc)
+            # 往四個方向移動中心點
+            dfs(r-1, c)
+            dfs(r, c-1)
+            dfs(r+1, c)
+            dfs(r, c+1)
 
         dfs(sr, sc)
         return image
